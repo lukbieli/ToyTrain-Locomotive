7f9d330bb17519a1d9223ccdae74b5c61762bff0
lukbieli <lbielinski01@gmail.com>
2025-05-04 01:09:37 +0200

    Update configuration for ESP32-C3 support and adjust motor control logic

--- main/battery_monitor.c
+++ main/battery_monitor.c
@@ -6,7 +6,15 @@
 #define TAG "BatteryMonitor"
 
 // ADC configuration
-#define BATTERY_ADC_CHANNEL ADC1_CHANNEL_6 // GPIO34
+#ifdef CONFIG_IDF_TARGET_ESP32
+#define BATTERY_ADC_CHANNEL ADC1_CHANNEL_6 // GPIO34 for ESP32-DevKit
+#elif CONFIG_IDF_TARGET_ESP32C3
+#define BATTERY_ADC_CHANNEL ADC1_CHANNEL_0 // GPIO0 for ESP32-C3
+#else
+#error "Unsupported target. Please define the target in the CMakeLists.txt file."
+#endif
+
+
 #define ADC_ATTENUATION ADC_ATTEN_DB_11    // Allows measurement up to ~3.6V
 #define ADC_WIDTH ADC_WIDTH_BIT_12         // 12-bit resolution
 #define BATTERY_VOLTAGE_MAX 4.2f // Maximum battery voltage (Li-ion cell)
@@ -17,6 +25,9 @@
 
 // ADC reference voltage (typically 3.3V for ESP32)
 #define ADC_REF_VOLTAGE 3.3
+
+static float battery_voltage = 0.0f; // Variable to store the battery voltage
+static uint8_t battery_level = 0;    // Variable to store the battery level percentage
 
 // Function to initialize the ADC
 void BatteryMonitor_Init(void)
@@ -40,7 +51,7 @@ float BatteryMonitor_Read(void)
     float measured_voltage = (raw_adc / 4095.0) * ADC_REF_VOLTAGE;
 
     // Adjust for the voltage divider
-    float battery_voltage = measured_voltage * VOLTAGE_DIVIDER_RATIO;
+    battery_voltage = measured_voltage * VOLTAGE_DIVIDER_RATIO;
 
     ESP_LOGI(TAG, "Raw ADC: %d, Measured Voltage: %.2fV, Battery Voltage: %.2fV",
              raw_adc, measured_voltage, battery_voltage);
@@ -53,8 +64,6 @@ float BatteryMonitor_Read(void)
 // 0% at 3.0V and 100% at 4.2V
 uint8_t BatteryMonitor_GetLevel(void)
 {
-    float battery_voltage = BatteryMonitor_Read();
-    uint8_t battery_level = 0;
 
     if (battery_voltage >= BATTERY_VOLTAGE_MAX) {
         battery_level = 100;
--- main/motor_driver.c
+++ main/motor_driver.c
@@ -19,24 +19,46 @@
 #include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/mcpwm_prelude.h"
+// #include "driver/mcpwm_prelude.h"
+#include "driver/ledc.h"
 #include "esp_log.h"
 #include "esp_err.h"
 
-#define SERVO_MIN_DEGREE        -90   // Minimum angle
-#define SERVO_MAX_DEGREE        90    // Maximum angle
-
+#ifdef CONFIG_IDF_TARGET_ESP32
+#include "driver/mcpwm.h"
+/* pinout for ESP32-Dev */
 #define PWM_GPIO             18        // GPIO connects to the PWM signal line
 #define MOT_IN1_GPIO        19        // GPIO connects to the motor driver IN1 pin
 #define MOT_IN2_GPIO        5        // GPIO connects to the motor driver IN2 pin
 #define MOT_STBY_GPIO      21        // GPIO connects to the motor driver STBY pin
+
 #define SERVO_TIMEBASE_RESOLUTION_HZ 1000000  // 1MHz, 1us per tick
 #define SERVO_TIMEBASE_PERIOD        1000    // 1000 ticks, 1ms
 #define SERVO_MIN_PULSEWIDTH_US 0  // Minimum pulse width in microsecond
 #define SERVO_MAX_PULSEWIDTH_US SERVO_TIMEBASE_PERIOD  // Maximum pulse width in microsecond
+#elif CONFIG_IDF_TARGET_ESP32C3
+#include "driver/ledc.h"
+/* pinout for ESP32-C3 */
+#define PWM_GPIO             21        // GPIO connects to the PWM signal line
+#define MOT_IN1_GPIO        10        // GPIO connects to the motor driver IN1 pin
+#define MOT_IN2_GPIO        20        // GPIO connects to the motor driver IN2 pin
+#define MOT_STBY_GPIO      9        // GPIO connects to the motor driver STBY pin
+
+#define LEDC_TIMER              LEDC_TIMER_0
+#define LEDC_MODE               LEDC_LOW_SPEED_MODE
+#define LEDC_CHANNEL            LEDC_CHANNEL_0
+#define LEDC_DUTY_RES           LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
+#define LEDC_DUTY_MAX               (8192) // Set duty to 50%. (2 ** 13) * 50% = 4096
+#define LEDC_FREQUENCY          (4000) // Frequency in Hertz. Set frequency at 4 kHz
+
+#else
+#error "Unsupported target. Please define the target in the CMakeLists.txt file."
+#endif
+
+
 
 static const char *TAG = "MOT_DRV";
-static mcpwm_cmpr_handle_t comparator = NULL;
+// static mcpwm_cmpr_handle_t comparator = NULL;
 
 /**
  * @brief Initializes the motor driver.
@@ -68,13 +90,13 @@ void MotorDriver_Init(void)
     ESP_ERROR_CHECK(gpio_set_level(MOT_IN2_GPIO, 0));
     ESP_LOGI(TAG, "Motor driver enabled and stopped");
     
-
+#ifdef CONFIG_IDF_TARGET_ESP32
     // initialize PWM output for motor control on pin 18
     ESP_LOGI(TAG, "Create timer and operator");
     mcpwm_timer_handle_t timer = NULL;
     mcpwm_timer_config_t timer_config = {
         .group_id = 0,
-        .clk_src = MCPWM_TIMER_CLK_SRC_DEFAULT,
+        .clk_src = MCPWM_TIMER_CLK_SRC_DEFAULT, // esp32 default clock source
         .resolution_hz = SERVO_TIMEBASE_RESOLUTION_HZ,
         .period_ticks = SERVO_TIMEBASE_PERIOD,
         .count_mode = MCPWM_TIMER_COUNT_MODE_UP,
@@ -117,6 +139,34 @@ void MotorDriver_Init(void)
     ESP_LOGI(TAG, "Enable and start timer");
     ESP_ERROR_CHECK(mcpwm_timer_enable(timer));
     ESP_ERROR_CHECK(mcpwm_timer_start_stop(timer, MCPWM_TIMER_START_NO_STOP));
+#elif CONFIG_IDF_TARGET_ESP32C3
+    // Initialize the LEDC peripheral for PWM control
+    // Prepare and then apply the LEDC PWM timer configuration
+    ledc_timer_config_t ledc_timer = {
+        .speed_mode       = LEDC_MODE,
+        .duty_resolution  = LEDC_DUTY_RES,
+        .timer_num        = LEDC_TIMER,
+        .freq_hz          = LEDC_FREQUENCY,  // Set output frequency at 4 kHz
+        .clk_cfg          = LEDC_AUTO_CLK
+    };
+    ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
+
+    // Prepare and then apply the LEDC PWM channel configuration
+    ledc_channel_config_t ledc_channel = {
+        .speed_mode     = LEDC_MODE,
+        .channel        = LEDC_CHANNEL,
+        .timer_sel      = LEDC_TIMER,
+        .intr_type      = LEDC_INTR_DISABLE,
+        .gpio_num       = PWM_GPIO,
+        .duty           = 0, // Set duty to 0%
+        .hpoint         = 0
+    };
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+
+#else
+    #error "Unsupported target. Please define the target in the CMakeLists.txt file."
+#endif
+
 }
 
 /**
@@ -130,13 +180,21 @@ void MotorDriver_Init(void)
 void MotorDriver_SetSpeed(uint8_t speed)
 {
     if (speed > 100) {
-        speed = 100; // Clamp speed to 100%
+        speed = 100; // Limit speed to 100%
     }
-
+#ifdef CONFIG_IDF_TARGET_ESP32
     // Set the PWM duty cycle based on the speed (0-100%)
     uint32_t pulse_width = SERVO_MIN_PULSEWIDTH_US + ((SERVO_MAX_PULSEWIDTH_US - SERVO_MIN_PULSEWIDTH_US) * speed) / 100;
     ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(comparator, pulse_width));
-    // ESP_LOGI(TAG, "Motor speed set to %d, pulse_width: %lu", speed, pulse_width);
+    ESP_LOGI(TAG, "Motor speed set to %d, pulse_width: %lu", speed, pulse_width);
+#elif CONFIG_IDF_TARGET_ESP32C3
+    // Set the PWM duty cycle based on the speed (0-100%)
+    uint32_t duty = (LEDC_DUTY_MAX * speed) / 100; // Calculate duty cycle based on speed (0-100%)
+    ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, duty)); // Set the duty cycle
+    ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL)); // Update the duty cycle  
+#else
+    #error "Unsupported target. Please define the target in the CMakeLists.txt file."
+#endif
 }
 
 /**
@@ -169,8 +227,8 @@ void MotorDriver_SetDirection(uint8_t direction)
  */
 void MotorDriver_Stop(void)
 {
-    // Set the PWM duty cycle to 0% to stop the motor
-    ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(comparator, SERVO_MIN_PULSEWIDTH_US));
+    // Set the PWM duty cycle to 0% to stop the motors
+    MotorDriver_SetSpeed(0);
 
     ESP_ERROR_CHECK(gpio_set_level(MOT_IN1_GPIO, 0));
     ESP_ERROR_CHECK(gpio_set_level(MOT_IN2_GPIO, 0));
